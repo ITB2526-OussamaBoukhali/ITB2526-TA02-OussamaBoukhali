a7e18aceec495044d74c12fb74b64191106e2335
oussama.boukhali.7e9 <oussama.boukhali.7e9@itb.cat>
2025-10-30 09:50:52 +0100

    IS PASSWORD OK

---
+++ TA04-OussamaBoukhali/isPasswordOK.py
@@ -0,0 +1,7 @@
+PWD = "asdasd"
+clave = input("Dime la contraseña")
+while clave != PWD:
+        print("Clave incorrecta!!!")
+        clave = input("Dime la clave")
+print("Bienvenido!!!")
+print("Programa terminado")
